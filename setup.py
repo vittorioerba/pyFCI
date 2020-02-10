c11af0959bd02bbe20875d9a426c6482242240ca
Vittorio Erba <erba.vittorio@gmail.com>
2020-02-10 11:30:53 +0100

    update

--- setup.py
+++ setup.py
@@ -17,6 +17,10 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ],
+        ],
+    install_requires=[
+        'numpy',
+        'scipy'
+        ],
     python_requires='>=3.6',
 )
